a7b7ccac41c8388d5385f120f97bf09938729919
Anuar Otynshin <strigeandro@gmail.com>
2018-05-14 21:04:37 +0600

    Finish module 16 (Transitions & Animations), 17 (Writing Future Proof CSS Code)

--- 2-2-hosting-website/shared.js
+++ 2-2-hosting-website/shared.js
@@ -4,11 +4,15 @@ var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan .button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
 	selectPlanButtons[i].addEventListener('click', function () {
 		modal.classList.add('open');
-		backdrop.classList.add('open');
+		backdrop.style.display = 'block';
+		setTimeout(function () {
+			backdrop.classList.add('open');
+		}, 10);
 	});
 }
 
@@ -26,9 +30,27 @@ function closeModal() {
 		modal.classList.remove('open');
 	}
 	backdrop.classList.remove('open');
+	setTimeout(function () {
+		backdrop.style.display = 'none';
+	}, 200);
 }
 
 toggleButton.addEventListener('click', function () {
 	mobileNav.classList.add('open');
-	backdrop.classList.add('open');
+	backdrop.style.display = 'block';
+	setTimeout(function () {
+		backdrop.classList.add('open');
+	}, 10);
+});
+
+ctaButton.addEventListener('animationstart', function (event) {
+	console.log('Animation started', event);
+});
+
+ctaButton.addEventListener('animationend', function (event) {
+	console.log('Animation ended', event);
+});
+
+ctaButton.addEventListener('animationiteration', function (event) {
+	console.log('Animation iterated', event);
 });
